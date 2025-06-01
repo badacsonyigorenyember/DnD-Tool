1ba5f540d2a7bb889b247a7a146ce3d8e5d86ac4
Tóth János <janos.toth.2000@gmail.com>
2025-06-01 15:14:52 +0200

    move camera controller to its own folder

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        public float Speed;
+        public float ZoomSpeed;
+        public float MinZoomValue;
+        public float MaxZoomValue;
+
+        private UnityEngine.Camera cam;
+
+        private void Start()
+        {
+            cam = GetComponent<UnityEngine.Camera>();
+        }
+
+        private void Update()
+        {
+            Move();
+            Zoom();
+        }
+
+        private void Move()
+        {
+            var xValue = Input.GetAxis("Horizontal");
+            var yValue = Input.GetAxis("Vertical");
+
+            Vector3 move = new Vector2(xValue, yValue);
+            transform.position += move * (Speed * Time.deltaTime);
+        }
+
+        private void Zoom()
+        {
+            var scrollValue = Input.GetAxis("Mouse ScrollWheel");
+
+            cam.orthographicSize += scrollValue * ZoomSpeed;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, MinZoomValue, MaxZoomValue);
+        }
+    }
+}
